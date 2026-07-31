8f75abb456f342aaa43680bf857ade5c6d24bf56
mrR <broitman.rom@gmail.com>
2026-07-31 17:26:46 +0300

    update start.log and babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,4 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-    plugins: [
-    'react-native-worklets/plugin', // add this, must be last
-  ],
+    plugins: ['react-native-worklets/plugin'],
 };
