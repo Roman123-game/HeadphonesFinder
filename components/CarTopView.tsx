049b0be6cb8d8f420bdb0ebc9e00c0e81b6e5eea
mrR <broitman.rom@gmail.com>
2026-07-31 15:47:29 +0300

    update CarTopView.tsx

--- components/CarTopView.tsx
+++ components/CarTopView.tsx
@@ -3,85 +3,241 @@
 import React from "react";
 import {
   View,
-  Image,
   StyleSheet,
-  ImageSourcePropType,
 } from "react-native";
+
 
 interface Props {
   children?: React.ReactNode;
   size?: number;
-  source?: ImageSourcePropType;
 }
+
 
 export default function CarTopView({
   children,
   size = 220,
-  source = require("../../assets/car_top.png"),
 }: Props) {
+
+
+  const radarSize = size * 1.7;
+
+
   return (
-    <View style={styles.container}>
-      {/* Radar Rings */}
 
-      <View style={styles.ringLarge} />
+    <View
+      style={[
+        styles.container,
+        {
+          width: radarSize,
+          height: radarSize,
+        },
+      ]}
+    >
 
-      <View style={styles.ringMedium} />
 
-      <View style={styles.ringSmall} />
+      {/* Radar */}
 
-      {/* Car */}
-
-      <Image
-        source={source}
-        style={{
-          width: size,
-          height: size,
-          resizeMode: "contain",
-        }}
+      <View
+        style={[
+          styles.ring,
+          {
+            width: radarSize,
+            height: radarSize,
+            borderRadius: radarSize / 2,
+          },
+        ]}
       />
 
-      {/* Headphone Dot */}
+
+      <View
+        style={[
+          styles.ring,
+          {
+            width: radarSize * 0.7,
+            height: radarSize * 0.7,
+            borderRadius: radarSize * 0.35,
+          },
+        ]}
+      />
+
+
+      <View
+        style={[
+          styles.ring,
+          {
+            width: radarSize * 0.4,
+            height: radarSize * 0.4,
+            borderRadius: radarSize * 0.2,
+          },
+        ]}
+      />
+
+
+
+      {/* Car body */}
+
+      <View
+        style={[
+          styles.car,
+          {
+            width:size * 0.55,
+            height:size,
+            borderRadius:size * 0.12,
+          },
+        ]}
+      >
+
+
+        {/* Front windshield */}
+
+        <View
+          style={[
+            styles.windshield,
+            {
+              width:size * 0.32,
+              height:size * 0.22,
+              top:size * 0.18,
+            },
+          ]}
+        />
+
+
+        {/* Rear window */}
+
+        <View
+          style={[
+            styles.windowBack,
+            {
+              width:size * 0.32,
+              height:size * 0.22,
+              bottom:size * 0.18,
+            },
+          ]}
+        />
+
+
+
+        {/* Wheels */}
+
+        <View style={[styles.wheel, styles.frontLeft]} />
+        <View style={[styles.wheel, styles.frontRight]} />
+
+        <View style={[styles.wheel, styles.backLeft]} />
+        <View style={[styles.wheel, styles.backRight]} />
+
+
+        {/* Lights */}
+
+        <View style={styles.lightFrontLeft}/>
+        <View style={styles.lightFrontRight}/>
+
+      </View>
+
+
+      {/* Headphone dot */}
 
       {children}
+
+
     </View>
   );
 }
 
+
+
 const styles = StyleSheet.create({
-  container: {
-    width: 380,
-    height: 380,
-    justifyContent: "center",
-    alignItems: "center",
+
+  container:{
+    justifyContent:"center",
+    alignItems:"center",
   },
 
-  ringLarge: {
-    position: "absolute",
-    width: 340,
-    height: 340,
-    borderRadius: 170,
-    borderWidth: 2,
-    borderColor: "#2ecc71",
-    opacity: 0.2,
+
+  ring:{
+    position:"absolute",
+    borderWidth:2,
+    borderColor:"#00ff88",
+    opacity:0.35,
   },
 
-  ringMedium: {
-    position: "absolute",
-    width: 240,
-    height: 240,
-    borderRadius: 120,
-    borderWidth: 2,
-    borderColor: "#2ecc71",
-    opacity: 0.4,
+
+  car:{
+    backgroundColor:"#1976d2",
+    position:"absolute",
+    justifyContent:"center",
+    alignItems:"center",
+
+    shadowColor:"#000",
+    shadowOpacity:0.5,
+    shadowRadius:10,
+    elevation:10,
   },
 
-  ringSmall: {
-    position: "absolute",
-    width: 140,
-    height: 140,
-    borderRadius: 70,
-    borderWidth: 2,
-    borderColor: "#2ecc71",
-    opacity: 0.7,
+
+  windshield:{
+    position:"absolute",
+    backgroundColor:"#90caf9",
+    borderRadius:10,
   },
+
+
+  windowBack:{
+    position:"absolute",
+    backgroundColor:"#64b5f6",
+    borderRadius:10,
+  },
+
+
+  wheel:{
+    position:"absolute",
+    width:18,
+    height:35,
+    backgroundColor:"#111",
+    borderRadius:8,
+  },
+
+
+  frontLeft:{
+    left:-8,
+    top:35,
+  },
+
+  frontRight:{
+    right:-8,
+    top:35,
+  },
+
+
+  backLeft:{
+    left:-8,
+    bottom:35,
+  },
+
+  backRight:{
+    right:-8,
+    bottom:35,
+  },
+
+
+  lightFrontLeft:{
+    position:"absolute",
+    top:5,
+    left:20,
+    width:15,
+    height:8,
+    backgroundColor:"#fff",
+    borderRadius:5,
+  },
+
+
+  lightFrontRight:{
+    position:"absolute",
+    top:5,
+    right:20,
+    width:15,
+    height:8,
+    backgroundColor:"#fff",
+    borderRadius:5,
+  },
+
 });
