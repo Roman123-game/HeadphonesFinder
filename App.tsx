f66bb6f46110710e83314120dd307ab02e4a0883
mrR <broitman.rom@gmail.com>
2026-08-01 08:30:49 +0300

    update 2 files and delete 10 files

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import CarTopView from './components/CarTopView';
+import CarTopView from './src/components/CarTopView';
 import styles from './App.styles';
 
 export default function App() {
